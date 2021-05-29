7aff0968c914aee7f41df97b71506410949af035
Francis Oyiogu <francis.oyiogu@decagon.dev>
2021-05-29 15:23:26 +0100

    created boiler plate for project

---
+++ src/main/java/com/decagon/safariwebstore/SafariWebStoreApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.safariwebstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafariWebStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafariWebStoreApplication.class, args);
+    }
+
+}
