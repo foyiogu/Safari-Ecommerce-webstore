43a61d093c018b948e313a0fd02ea994ef1c925f
adenusidamilola5@gmail.com <adenusidamilola5@gmail.com>
2021-06-11 21:15:23 +0100

    admin forgot password refactored

---
+++ src/main/java/com/decagon/safariwebstore/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.decagon.safariwebstore.controller;
+
+import com.decagon.safariwebstore.exceptions.ResourceNotFoundException;
+import com.decagon.safariwebstore.model.ERole;
+import com.decagon.safariwebstore.model.Role;
+import com.decagon.safariwebstore.payload.response.Response;
+import com.decagon.safariwebstore.payload.response.auth.ResetPassword;
+import com.decagon.safariwebstore.repository.RoleRepository;
+import com.decagon.safariwebstore.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @PostMapping("/admin/password-forgot")
+    public ResponseEntity<Response> adminForgotPassword(@RequestParam("email") String email, HttpServletRequest req){
+
+        Role adminRole = roleRepository.findByName(ERole.ADMIN)
+                .orElseThrow(() -> new ResourceNotFoundException("Error: Role is not found."));
+
+        return adminService.adminForgotPassword(req, adminRole, email);
+    }
+
+    @PostMapping("/admin/password-reset")
+    public ResponseEntity<Response> adminResetPassword(@Valid @RequestBody ResetPassword resetPassword) {
+        return adminService.adminResetPassword(resetPassword);
+    }
+}
+
---
+++ src/main/java/com/decagon/safariwebstore/service/serviceImplementation/AdminServiceImplementation.java
@@ -0,0 +1,156 @@
+package com.decagon.safariwebstore.service.serviceImplementation;
+
+import com.decagon.safariwebstore.model.Role;
+import com.decagon.safariwebstore.model.User;
+import com.decagon.safariwebstore.payload.response.Response;
+import com.decagon.safariwebstore.payload.response.auth.ResetPassword;
+import com.decagon.safariwebstore.service.AdminService;
+import com.decagon.safariwebstore.service.UserService;
+import com.decagon.safariwebstore.utils.DateUtils;
+import com.decagon.safariwebstore.utils.mailService.MailService;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AdminServiceImplementation implements AdminService {
+
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private MailService mailService;
+
+    @Autowired
+    public AdminServiceImplementation(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, MailService mailService){
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.mailService = mailService;
+    }
+
+    /**
+     * Sends an email to the admin with a url link to reset password
+     * @param req
+     * @param adminRole
+     * @param email
+     * */
+    @Override
+    public ResponseEntity<Response> adminForgotPassword(HttpServletRequest req, Role adminRole, String email) {
+        // Lookup user in database by e-mail
+        Optional<User> adminOptional = userService.getUserByEmail(email);
+
+        //response handler
+        Response res = new Response();
+
+        if(adminOptional.isEmpty()) {
+            res.setStatus(404);
+            res.setMessage("We couldn't find an account with that e-mail address.");
+
+            return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+        }
+
+        Role role = adminOptional.get().getRoles().get(0);
+
+        if(role != adminRole){
+            res.setStatus(401);
+            res.setMessage("You don't have access to this link");
+
+            return new ResponseEntity<>(res, HttpStatus.UNAUTHORIZED);
+        }
+
+        try{
+            // Generate random 36-character string token for reset password
+            User admin = adminOptional.get();
+
+            admin.setPasswordResetToken(UUID.randomUUID().toString());
+
+            //24hours expiry date for token
+            String tokenExpiryDate = DateUtils.passwordResetExpiryTimeLimit();
+
+            admin.setPasswordResetExpireDate(tokenExpiryDate);
+
+            String appUrl = req.getScheme() + "://" + req.getServerName();
+
+            String subject = "Admin Reset Password";
+
+            String mailBody = "To reset your password, click the link below:\n"
+                    + appUrl + "/reset?token="
+                    + admin.getPasswordResetToken();
+
+            mailService.sendMessage(admin.getEmail(), subject, mailBody);
+
+            // Save token and expiring date to database
+            userService.saveUser(admin);
+
+            res.setStatus(200);
+            res.setMessage("Successfully sent email");
+
+        }catch (UnirestException e){
+            System.out.println("Error sending email:\n\tError message:"+e.getMessage());
+        }
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    /**
+     * This method check the validity of the token sent and also validates passwords(password and confirm password)
+     * before updating it
+     * @param resetPassword
+     * @return response
+     * */
+    @Override
+    public ResponseEntity<Response> adminResetPassword(ResetPassword resetPassword) {
+
+        //find the admin by the token
+        Optional<User> adminOptional = userService.findUserByResetToken(resetPassword.getToken());
+
+        String password = resetPassword.getPassword();
+        String confirmPassword = resetPassword.getConfirmPassword();
+
+        //response handler
+        Response res = new Response();
+
+        if (adminOptional.isEmpty()){
+            res.setStatus(400);
+            res.setMessage("Oops!  This is an invalid password reset link.");
+
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+        }
+
+        User admin = adminOptional.get();
+
+        if(!password.equals(confirmPassword)){
+            res.setStatus(400);
+            res.setMessage("Passwords does not match");
+
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+        }
+
+        //set the encrypted password
+        admin.setPassword(bCryptPasswordEncoder.encode(password));
+
+        // Set the reset token to null so it cannot be used again
+        admin.setPasswordResetToken(null);
+
+        //set the reset passwordRestExpireDate to null
+        admin.setPasswordResetExpireDate(null);
+
+        try {
+            // Save person
+            userService.saveUser(admin);
+
+            res.setStatus(201);
+            res.setMessage("You have successfully reset your password. You can now login.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+}
+
